440a412df869e81f8e8ab5753343b96ac05f4b80
Minh Nguyen <121152335+Minh111104@users.noreply.github.com>
2025-04-02 23:11:57 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import BirthdayCountdown from "./BirthdayCountdown";
+
+function App() {
+  return (
+    <div className="App">
+      <BirthdayCountdown />
+    </div>
+  );
+}
+
+export default App;
